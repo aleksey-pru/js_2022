42bd42eca54630afed18571b3d59a0d2221d43a6
Alex <o.pruzhanskiy@gmail.com>
2022-03-21 12:10:13 +0200

    added js and html files

--- project.js
+++ project.js
@@ -1,1 +1,16 @@
 console.log('dfdg..');
+
+const button = document.getElementById('add_action');
+
+button.addEventListener('click',() => {
+    console.log('CLICKED');
+})
+button.addEventListener('mouseover',() => {
+    console.log('MOUSEOVER');
+})
+button.addEventListener('focus',() => {
+    console.log('FOCUS');
+})
+button.addEventListener('blur',() => {
+    console.log('BLUR');
+})
